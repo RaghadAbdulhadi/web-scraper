123d8459d0b772bc7d10f87a3f1c7e4d3b8cf472
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-04-16 03:11:16 +0300

    Final Count, Report Function

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -1,13 +1,14 @@
 from web_scraper.scraper import get_citations_needed_count, get_citations_needed_report_testing
 
 wiki_url = "https://en.wikipedia.org/wiki/History_of_Mexico"
+
+# Check the number of returned paragraphs
 def test_get_citations_needed_count():
     actual = get_citations_needed_count(wiki_url)
     expected = 5 
     assert actual == expected
 
-
-
+# Check if the first returned string is the firs found paragraph in the website
 def test_get_citations_needed_report():
     actual = get_citations_needed_report_testing(wiki_url)
     expected = "The first people to settle in Mexico encountered a climate far milder than the current one. In particular, the Valley of Mexico contained several large paleo-lakes (known collectively as Lake Texcoco) surrounded by dense forest. Deer were found in this area, but most fauna were small land animals and fish and other lacustrine animals were found in the lake region.[citation needed][6] Such conditions encouraged the initial pursuit of a hunter-gatherer existence."
